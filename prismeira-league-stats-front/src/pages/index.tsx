142b3ce19a86d3a679a139cfadca237d21136777
GregArendXavier <90474008+GregArendXavier@users.noreply.github.com>
2024-02-16 12:47:16 -0300

    alter 16.02 v3

--- prismeira-league-stats-front/src/pages/index.tsx
+++ prismeira-league-stats-front/src/pages/index.tsx
@@ -102,7 +102,8 @@ export default function Home() {
           </div>
         : null}
         <span className='dkp:text-8xl text-white bg-gray-800 mx-4 dkp:py-8 dkp:px-10 rounded-lg border-2 border-white cel:p-3 cel:text-xl' style={{
-          boxShadow: "15px 15px 15px #000"
+          boxShadow: "15px 15px 15px #000",
+          color: '#fff'
         }}>
           {dias < 10 ? `0${dias}` : dias}
         </span>
