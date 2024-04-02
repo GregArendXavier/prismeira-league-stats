6875fad248413bbb59a0a394f361e49c37525613
GregArendXavier <90474008+GregArendXavier@users.noreply.github.com>
2024-04-02 14:54:12 -0300

    alter 02.04 v2

--- prismeira-league-stats-api/src/database/conection.mjs
+++ prismeira-league-stats-api/src/database/conection.mjs
@@ -3,7 +3,7 @@ import knex from "knex";
 const database = knex({
   client: "sqlite3",
   connection: {
-    filename: "data.db",
+    filename: "./src/data.db",
   },
   useNullAsDefault: true,
 });
